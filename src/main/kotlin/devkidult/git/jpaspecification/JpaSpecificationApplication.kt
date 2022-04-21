329403baf85547e11c7d9a271717a498b621b895
YH <yh3488@adwitt.com>
2022-04-21 15:24:14 +0900

    init project

---
+++ src/main/kotlin/devkidult/git/jpaspecification/JpaSpecificationApplication.kt
@@ -0,0 +1,11 @@
+package devkidult.git.jpaspecification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaSpecificationApplication
+
+fun main(args: Array<String>) {
+    runApplication<JpaSpecificationApplication>(*args)
+}
---
+++ src/test/kotlin/devkidult/git/jpaspecification/JpaSpecificationApplicationTests.kt
@@ -0,0 +1,13 @@
+package devkidult.git.jpaspecification
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaSpecificationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
